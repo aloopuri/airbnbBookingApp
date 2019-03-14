a56f019617de5db91b02108990033ab56d0e0948
SagekingRoco <774677341@qq.com>
2019-03-14 20:26:40 +0000

    MapView now stays at the center
    But I couldn't add ScrollPane...Need help to fix it

---
+++ ListingsEachBorough.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.*;
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import java.net.URL;
+import javafx.fxml.*;
+import javafx.scene.paint.*;
+
+public class ListingsEachBorough extends Application
+{
+  @Override
+  public void start(Stage stage) throws Exception
+  {
+    URL url = getClass().getResource("ListingsInBoroughView.fxml");
+    Parent root = FXMLLoader.load(url);
+    Scene scene = new Scene(root);
+
+    stage.setTitle("Map");
+    stage.setScene(scene);
+    stage.show();
+  }
+}
