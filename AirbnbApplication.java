fabe64e756959ba613afeb1933acc6f93414dfc1
Hassan, Mohammad (k1889317) <mohammad.hassan@kcl.ac.uk>
2019-03-26 01:22:42 +0000

    Add files via upload

--- AirbnbApplication.java
+++ AirbnbApplication.java
@@ -1,45 +1,61 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
+import javafx.event.*;
+import javafx.scene.*;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.collections.*;
-
+import javafx.animation.*;
+import javafx.util.Duration;
+import javafx.scene.image.*;
 import java.util.ArrayList;
+
 /**
- * Write a description of JavaFX class Application here.
+ * Write a description of class AirbnbApp here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
 public class AirbnbApplication extends Application
 {
-    private Label myLabel = new Label("Welcome"); // Temporary
-    
-    private ArrayList<Scene> scenes;
+    private ArrayList<Pane> panels;
+    private Pane currentPanel;
+    private ListingManager listingManager;
     private ArrayList<AirbnbListing> listings;
+    private int panelIndex;
     
-    // Controls on most panels
+    private BorderPane main;
     private Button backButton = new Button("<");
     private Button frontButton = new Button(">");
     private ComboBox fromBox;
     private ComboBox toBox;
+    private HBox topPane;
+    private BorderPane bottomPane;
+    
+    private WelcomePanel welcomePanel;
+    private MapPanel map;
+    private StatisticPanel statsPanel;
     
     /**
-     * Class Constructor
+     * Constructor for objects of class AirbnbApp
      */
-    public AirbnbApplication() 
+    public AirbnbApplication()
     {
-        scenes = new ArrayList<Scene>();
+        panels = new ArrayList<>();
         listings = new ArrayList<AirbnbListing>();
         AirbnbDataLoader loader = new AirbnbDataLoader();
-        listings = loader.load();          
+        listings = loader.load();
+        listingManager = new ListingManager(listings);
+        main = new BorderPane();
         fromBox = new ComboBox(getOptionsList());
         toBox = new ComboBox(getOptionsList());
+        //int i = 0;
+        // while (i < listings.size()){
+            // System.out.println(listings.get(i).getNeighbourhood());
+            // i++;
+        // };
     }
     
     /**
@@ -48,107 +64,149 @@ public class AirbnbApplication extends Application
     @Override
     public void start(Stage stage) throws Exception
     {
+        welcomePanel = new WelcomePanel();       
+        map = new MapPanel(listingManager);
+        statsPanel = new StatisticPanel(listings);
+        
+        panels.add(welcomePanel.getWelcomePanel());
+        panels.add(map.createMap());
+        panels.add(statsPanel.getStatisticPanel());
+        
+
+        Image icon = new Image("/images/airbnb-small.png");
+        ImageView iconView = new ImageView();
+        iconView.setImage(icon);
+        iconView.setPreserveRatio(true);
+        iconView.setFitHeight(60);
+        iconView.setFitWidth(60);
+        
+        Region leftRegion = new Region();
+        
         Label fromLabel = new Label("From:");
         Label toLabel = new Label("To:");
-
-        fromBox.setPromptText("-");
-        toBox.setPromptText("-");
-        
-        backButton.setPrefWidth(50);
-        frontButton.setPrefWidth(50);
-        backButton.setDisable(true);
-        frontButton.setDisable(true);
-        
-        // Create a new HBox
-        HBox topPane = new HBox();
-        topPane.setAlignment(Pos.CENTER_RIGHT);
-        topPane.setSpacing(10);
-        topPane.getChildren().add(fromLabel);
-        topPane.getChildren().add(fromBox);
-        topPane.getChildren().add(toLabel);
-        topPane.getChildren().add(toBox);
-        
-        BorderPane bottomPane = new BorderPane();
-        bottomPane.setRight(frontButton);
-        bottomPane.setLeft(backButton);
-
-        // Create a new border pane
-        BorderPane root = new BorderPane();
-        root.setTop(topPane);
-        root.setCenter(myLabel);
-        root.setBottom(bottomPane);
-        
-        // JavaFX must have a Scene (window content) inside a Stage (window)
-        Scene welcomeScene = new Scene(root, 500, 500);
-        scenes.add(welcomeScene);
-        stage.setTitle("Airbnb London");
-        stage.setScene(welcomeScene);
         
         // Set ComboBox actions
         fromBox.setOnAction(e -> comboBoxAction());
-        toBox.setOnAction(e -> comboBoxAction());
+        toBox.setOnAction(e -> comboBoxAction());  
         
+        fromBox.setPromptText("-");
+        fromBox.setId("fromBox");
+        toBox.setPromptText("-");
+        toBox.setId("toBox");
+
+        topPane = new HBox();
+        topPane.setAlignment(Pos.CENTER_RIGHT);
+        topPane.setSpacing(10);
+        topPane.getChildren().addAll(iconView, leftRegion, fromLabel, fromBox, toLabel, toBox, new Region());
+        topPane.setHgrow(leftRegion, Priority.ALWAYS);
+        
+        frontButton.setId("frontButton");
+        backButton.setId("backButton");
+        backButton.setPrefWidth(50);
+        frontButton.setPrefWidth(50);
+        disableNavigation();
+
+        frontButton.setOnAction(e -> nextPanel(e));
+        backButton.setOnAction(e -> previousPanel(e));
+        
+        bottomPane = new BorderPane();
+        bottomPane.setRight(frontButton);
+        bottomPane.setLeft(backButton);
+        //bottomPane.setMinHeight(null);
+
+        main.setCenter(panels.get(0));
+        main.setTop(topPane);        
+        main.setBottom(bottomPane);        
+        
+        Scene scene = new Scene(main, 1200, 800);
+        stage.setTitle("Airbnb London");
+        stage.setScene(scene);
+        scene.getStylesheets().add("WelcomeLayout.css");
+
         // Show the Stage (window)
         stage.show();
+        stage.getIcons().add(new Image("/images/airbnb-small.png"));
     }
-    
+
     /**
      * Performs the ComboBox actions when interacted with
      */
-    private void comboBoxAction() 
+    private void comboBoxAction()
     {
         if (fromBox.getValue() != null && toBox.getValue() != null) {
-            Integer toValue = (Integer)toBox.getValue();
-            Integer fromValue = (Integer)fromBox.getValue();
+            Integer toValue = (Integer) toBox.getValue();
+            Integer fromValue = (Integer) fromBox.getValue();
             int to = toValue.intValue();
             int from = fromValue.intValue();
             if (to < from) {
                 toBox.setValue(null);
-                fromBox.setValue(null);                
+                fromBox.setValue(null);
                 Alert alert = new Alert(AlertType.WARNING);
                 alert.setTitle("Value Warning");
-                alert.setHeaderText(null);
                 alert.setContentText("From value is greater than To value.");
-                alert.showAndWait();
+                welcomePanel.updatePriceRange(fromBox.getValue(), toBox.getValue());
                 disableNavigation();
+                map.updateView(0, listingManager.getListings().size());
+                alert.showAndWait();
             }
             else {
+                welcomePanel.updatePriceRange(fromBox.getValue(), toBox.getValue());
+                listingManager.updateUserRangeListings(from, to);
+                //System.out.println(listingManager.getCurrentListings().size());
+                map.updateView(from, to);
+                statsPanel.updateStatistics(listingManager.getCurrentListings());
                 enableNavigation();
             }
         }
     }
     
     /**
-     * Sets up and returns the list of options for use in combo boxes
-     * @return options The list of combo box options
-     */
-    private ObservableList<Integer> getOptionsList() 
-    {
-        ObservableList<Integer> options = FXCollections.observableArrayList();
-        
-        for (AirbnbListing aListing : listings) {
-            options.add(aListing.getPrice());
-        }
-        FXCollections.sort(options);
-        
-        return options;
-    }
-    
-    /**
      * Enables the navigation buttons at the bottom of the window
      */
-    private void enableNavigation() 
+    private void enableNavigation()
     {
         frontButton.setDisable(false);
-        backButton.setDisable(false);        
+        backButton.setDisable(false);
     }
-    
+
     /**
      * Disables the navigation buttons at the bottom of the window
      */
-    private void disableNavigation() 
+    private void disableNavigation()
     {
         frontButton.setDisable(true);
         backButton.setDisable(true);
+    }
+        
+    private ObservableList<Integer> getOptionsList()
+    {
+        ObservableList<Integer> options = FXCollections.observableArrayList(listingManager.getMenuOptions());
+        FXCollections.sort(options);
+        return options;
+    }
+    
+    private void nextPanel(ActionEvent event)
+    {
+        panelIndex++;
+        if (panelIndex >= panels.size()) {
+            panelIndex = 0;
+        }
+        //panelIndex = panelIndex % panels.size();
+        changePanel(panelIndex);
+    }
+
+    private void previousPanel(ActionEvent event)
+    {
+        panelIndex--;
+        if (panelIndex < 0) {
+            panelIndex = panels.size() - 1;
+        }
+        changePanel(panelIndex);
+    }
+    
+    private void changePanel(int panelIndex)
+    {
+        main.setCenter(null);
+        main.setCenter(panels.get(panelIndex));
     }
 }
