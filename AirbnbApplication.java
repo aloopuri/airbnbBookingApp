406175ac357418b4e4513430d8d90899477696e8
SagekingRoco <774677341@qq.com>
2019-03-28 21:44:00 +0000

    animation added when changing panel

--- AirbnbApplication.java
+++ AirbnbApplication.java
@@ -11,6 +11,8 @@ import javafx.animation.*;
 import javafx.util.Duration;
 import javafx.scene.image.*;
 import java.util.ArrayList;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 /**
  * Write a description of JavaFX class Application here.
  *
@@ -22,9 +24,9 @@ public class AirbnbApplication extends Application
     private ArrayList<Pane> panels;
     private Pane currentPanel;
     private int panelIndex;
-    
+    private double animationIndex;
     private ListingManager listingManager;
-    
+
     // Controls on most panels
     private BorderPane main;
     private Button backButton = new Button("<");
@@ -33,11 +35,11 @@ public class AirbnbApplication extends Application
     private static ComboBox toBox;
     private HBox topPane;
     private BorderPane bottomPane;
-    
+
     // Application panels
     private WelcomePanel welcomePanel;
     private MapPanel map;
-    
+
     /**
      * Class Constructor
      */
@@ -48,13 +50,13 @@ public class AirbnbApplication extends Application
         AirbnbDataLoader loader = new AirbnbDataLoader();
         listings = loader.load();
         listingManager = new ListingManager(listings);
-        
+
         main = new BorderPane();
         main.setId("welcomePane");
-        
+
         fromBox = new ComboBox(getOptionsList());
         fromBox.setCellFactory(c -> new FromBoxCell());
-        
+
         toBox = new ComboBox(getOptionsList());
         toBox.setCellFactory(c -> new ToBoxCell());
     }
@@ -67,10 +69,10 @@ public class AirbnbApplication extends Application
     {
         welcomePanel = new WelcomePanel();
         map = new MapPanel(listingManager);
-        
+
         panels.add(welcomePanel.getWelcomePanel());
         panels.add(map.createMap());
-        
+
         // Set ComboBox actions
         fromBox.setOnAction(e -> comboBoxAction());
         toBox.setOnAction(e -> comboBoxAction());
@@ -84,16 +86,16 @@ public class AirbnbApplication extends Application
         iconView.setPreserveRatio(true);
         iconView.setFitHeight(60);
         iconView.setFitWidth(60);
-        
+
         Region leftRegion = new Region();
-        
+
         Label fromLabel = new Label("From:");
         Label toLabel = new Label("To:");
-        
+
         // Set ComboBox actions
         fromBox.setOnAction(e -> comboBoxAction());
-        toBox.setOnAction(e -> comboBoxAction());  
-        
+        toBox.setOnAction(e -> comboBoxAction());
+
         fromBox.setPromptText("-");
         fromBox.setId("fromBox");
         toBox.setPromptText("-");
@@ -104,24 +106,24 @@ public class AirbnbApplication extends Application
         topPane.setSpacing(10);
         topPane.getChildren().addAll(iconView, leftRegion, fromLabel, fromBox, toLabel, toBox, new Region());
         topPane.setHgrow(leftRegion, Priority.ALWAYS);
-        
+
         frontButton.setId("frontButton");
         backButton.setId("backButton");
         backButton.setPrefWidth(50);
         frontButton.setPrefWidth(50);
-        disableNavigation();
+        setNavigationUsability(true);
 
         frontButton.setOnAction(e -> nextPanel(e));
         backButton.setOnAction(e -> previousPanel(e));
-        
+
         bottomPane = new BorderPane();
         bottomPane.setRight(frontButton);
         bottomPane.setLeft(backButton);
 
         main.setCenter(panels.get(0));
-        main.setTop(topPane);        
-        main.setBottom(bottomPane);        
-        
+        main.setTop(topPane);
+        main.setBottom(bottomPane);
+
         Scene scene = new Scene(main, 1200, 900);
         stage.setTitle("Airbnb London");
         stage.setScene(scene);
@@ -138,13 +140,13 @@ public class AirbnbApplication extends Application
      * Performs the ComboBox actions when interacted with
      */
     private void comboBoxAction()
-    {   
+    {
         if (fromBox.getValue() != null && toBox.getValue() != null) {
             int to = getToValue();
             int from = getFromValue();
             welcomePanel.updatePriceRange(fromBox.getValue(), toBox.getValue());
             map.showViewInRange(from, to);
-            enableNavigation();
+            setNavigationUsability(false);
         }
     }
 
@@ -160,52 +162,79 @@ public class AirbnbApplication extends Application
     }
 
     /**
-     * Enables the navigation buttons at the bottom of the window
+     * determine whether the navigation buttons are usable at the bottom of the window
+     * passing a true as the parameter sets navigation buttons disabled,
+     * passing a false sets them usable.
      */
-    private void enableNavigation()
+    private void setNavigationUsability(boolean booleanValue)
     {
-        frontButton.setDisable(false);
-        backButton.setDisable(false);
+        frontButton.setDisable(booleanValue);
+        backButton.setDisable(booleanValue);
     }
 
     /**
-     * Disables the navigation buttons at the bottom of the window
+     * set up an action for the front button
      */
-    private void disableNavigation()
-    {
-        frontButton.setDisable(true);
-        backButton.setDisable(true);
-    }
-
     private void nextPanel(ActionEvent event)
     {
         panelIndex++;
+        //wrap around the panel
         if (panelIndex >= panels.size()) {
             panelIndex = 0;
         }
-        //panelIndex = panelIndex % panels.size();
+        //the next panel then goes from right to the center
+        animationIndex = 2000f;
         changePanel(panelIndex);
     }
 
+    /**
+     * set up an action for the previous button
+     */
     private void previousPanel(ActionEvent event)
     {
         panelIndex--;
+        //wrap around the panel
         if (panelIndex < 0) {
             panelIndex = panels.size() - 1;
         }
+        //the previous panel then goes from left to the center
+        animationIndex = -2000f;
         changePanel(panelIndex);
     }
 
+    /**
+     * get the next or previous panel from the list and
+     * it as the center pane
+     */
     private void changePanel(int panelIndex)
     {
         main.setCenter(null);
+        changingPanelAnimation(panels.get(panelIndex));
         main.setCenter(panels.get(panelIndex));
     }
-    
+
+    /**
+     * set up the animation for the panel
+     * when the center pane is changed to it
+     */
+    private void changingPanelAnimation(Pane pane)
+    {
+     final Duration TIME = Duration.millis(750);
+
+     TranslateTransition tt = new TranslateTransition(TIME);
+     tt.setFromX(animationIndex);
+     tt.setToX(25f);
+     tt.setCycleCount(1);
+     tt.setAutoReverse(false);
+
+     ParallelTransition pt = new ParallelTransition(pane,tt);
+     pt.play();
+    }
+
     /**
      * @return The int value of the selected FromBox value
      */
-    public static int getFromValue() 
+    public static int getFromValue()
     {
         if (fromBox.getValue() != null) {
             Integer fromValue = (Integer) fromBox.getValue();
@@ -216,11 +245,11 @@ public class AirbnbApplication extends Application
             return -1;
         }
     }
-    
+
     /**
      * @return The int value of the selected ToBox value
      */
-    public static int getToValue() 
+    public static int getToValue()
     {
         if (toBox.getValue() != null) {
             Integer toValue = (Integer) toBox.getValue();
@@ -231,25 +260,25 @@ public class AirbnbApplication extends Application
             return -1;
         }
     }
-    
+
     /**
      * FromBox cell factory class
      */
     private class FromBoxCell extends ListCell<Integer> {
         /**
-         * Adds listener that listens for a change in the value of the toBox 
+         * Adds listener that listens for a change in the value of the toBox
          * and updates the relevant disabled cells
          */
-        private FromBoxCell() 
+        private FromBoxCell()
         {
             toBox.valueProperty().addListener((change, oldValue, newValue) -> updateDisabled());
         }
-        
+
         /**
          * Updates each cell's appearance
          */
         @Override
-        protected void updateItem(Integer item, boolean empty) 
+        protected void updateItem(Integer item, boolean empty)
         {
             super.updateItem(item, empty);
             if (item != null) {
@@ -259,27 +288,27 @@ public class AirbnbApplication extends Application
                 setText(null);
             }
         }
-        
+
         /**
          * Updates the disabled property of a cell
          */
-        private void updateDisabled() 
+        private void updateDisabled()
         {
             boolean isDisabled = false;
             if (getItem() != null && toBox.getValue() != null) {
                 isDisabled = getItem().intValue() > (Integer)toBox.getValue();
             }
             setDisable(isDisabled);
-        }        
+        }
     }
-    
+
     /**
      * ToBox cell factory class
      */
-    private class ToBoxCell extends ListCell<Integer> 
+    private class ToBoxCell extends ListCell<Integer>
     {
         /**
-         * Adds listener that listens for a change in the value of the toBox 
+         * Adds listener that listens for a change in the value of the toBox
          * and updates the relevant disabled cells
          */
         private ToBoxCell()
@@ -300,7 +329,7 @@ public class AirbnbApplication extends Application
                 setText(null);
             }
         }
-        
+
         /**
          * Updates the disabled property of a cell
          */
@@ -310,6 +339,6 @@ public class AirbnbApplication extends Application
                 isDisabled = getItem().intValue() < (Integer)fromBox.getValue();
             }
             setDisable(isDisabled);
-        }        
+        }
     }
 }
