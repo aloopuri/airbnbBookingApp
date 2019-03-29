ffffd75cfda9a6f47d04e0257cf83bb204475f72
Raja, Zain (k1801352) <zain.raja@kcl.ac.uk>
2019-03-29 18:29:48 +0000

    all panels + buttons appear

--- MapPanelEngine.java
+++ MapPanelEngine.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 import javafx.collections.*;
 import javafx.scene.control.*;
 import javafx.scene.control.TableColumn.SortType;
+import javafx.collections.transformation.*;
 public class MapPanelEngine
 {
     private Button button;
-    private Color color;
     private BackgroundFill backgroundFill;
     private Background background;
     public String getBoroughName(Button button)
@@ -43,80 +43,78 @@ public class MapPanelEngine
       }
     }
 
-    public void getButtonColor(Button button, int listingCount)
+    public void setButtonColor(Button button, int listingCount)
     {
-      if(listingCount > 300 & listingCount <= 600)
-      {
-        color = Color.web("#ADD8E6");
-      }
-      else if(listingCount > 600 & listingCount <= 1000)
-      {
-        color = Color.web("#87CEEB");
-      }
-      else if(listingCount > 1000 & listingCount <= 1500)
-      {
-        color = Color.web("#87CEFA");
-      }
-      else if(listingCount > 1500 & listingCount <= 2000)
-      {
-        color = Color.web("#00BFFF");
-      }
-      else if(listingCount > 2000 & listingCount <= 2500)
-      {
-        color = Color.web("#1E90FF");
-      }
-      else if(listingCount > 2500 & listingCount <= 3500)
-      {
-        color = Color.web("#6495ED");
-      }
-      else if(listingCount >= 0 & listingCount <= 10 )
+      Color color;
+      if(listingCount >= 0 && listingCount <= 10 )
       {
         color = Color.web("#E0FFFF");
       }
-      else if(listingCount > 10 & listingCount <= 20)
+      else if(listingCount > 10 && listingCount <= 20)
       {
         color = Color.web("#AFEEEE");
       }
-      else if(listingCount > 20 & listingCount <= 30)
+      else if(listingCount > 20 && listingCount <= 30)
       {
         color = Color.web("#7FFFD4");
       }
-      else if(listingCount > 30 & listingCount <= 40)
+      else if(listingCount > 30 && listingCount <= 40)
       {
         color = Color.web("#40E0D0");
       }
-      else if(listingCount > 40 & listingCount <= 50)
+      else if(listingCount > 40 && listingCount <= 50)
       {
         color = Color.web("#48D1CC");
       }
-      else if(listingCount > 50 & listingCount <= 100)
+      else if(listingCount > 50 && listingCount <= 100)
       {
         color = Color.web("#00CED1");
       }
-      else if(listingCount > 100 & listingCount <= 150)
+      else if(listingCount > 100 && listingCount <= 150)
       {
         color = Color.web("#5F9EA0");
       }
-      else if(listingCount > 150 & listingCount <= 200)
+      else if(listingCount > 150 && listingCount <= 200)
       {
         color = Color.web("#4682B4");
       }
-      else if(listingCount > 200 & listingCount <= 250)
+      else if(listingCount > 200 && listingCount <= 250)
       {
         color = Color.web("#B0C4DE");
       }
-      else if(listingCount > 250 & listingCount <= 300)
+      else if(listingCount > 250 && listingCount <= 300)
       {
         color = Color.web("#B0E0E6");
+      }
+      else if(listingCount > 300 && listingCount <= 600)
+      {
+        color = Color.web("#ADD8E6");
+      }
+      else if(listingCount > 600 && listingCount <= 1000)
+      {
+        color = Color.web("#87CEEB");
+      }
+      else if(listingCount > 1000 && listingCount <= 1500)
+      {
+        color = Color.web("#87CEFA");
+      }
+      else if(listingCount > 1500 && listingCount <= 2000)
+      {
+        color = Color.web("#00BFFF");
+      }
+      else if(listingCount > 2000 && listingCount <= 2500)
+      {
+        color = Color.web("#1E90FF");
+      }
+      else if(listingCount > 2500 && listingCount <= 3500)
+      {
+        color = Color.web("#6495ED");
       }
       else
       {
         color = Color.web("#4169E1");
       }
-    }
 
-    public  void setButtonColor(Button button)
-    {
       backgroundFill = new BackgroundFill(color, null, null);
       background = new Background(backgroundFill);
       button.setBackground(background);
@@ -134,6 +132,37 @@ public class MapPanelEngine
                     "Number of Reviews"
         );
         return sortOptions;
+    }
+
+    /**
+     * Implements search functionality to search the Borough Table
+     * Reference: https://code.makery.ch/blog/javafx-8-tableview-sorting-filtering/
+     */
+    public void searchTable(ListingManager listingManager, String boroughName, TextField searchText, TableView table)
+    {
+        // Wrap the ObservableList in a FilteredList (initially display all data).
+        FilteredList<AirbnbListing> filteredData = new FilteredList<>(listingManager.getBoroughListings(boroughName, AirbnbApplication.getFromValue(), AirbnbApplication.getToValue()), p -> true);
+
+        // Set the filter Predicate whenever the filter changes.
+        searchText.textProperty().addListener((change, oldValue, newValue) -> {
+            filteredData.setPredicate(airbnbListing -> {
+                // Compare host name of every property with filter text.
+                String lowerCaseFilter = newValue.toLowerCase();
+                // If filter text is empty, display all properties.
+                return (newValue == null || newValue.isEmpty()
+                        //if filter text contains or euquals to the property info, display the list
+                        ||airbnbListing.getHost_name().toLowerCase().contains(lowerCaseFilter))
+                        || (Integer.toString(airbnbListing.getPrice()).equals(lowerCaseFilter))
+                        || (Integer.toString(airbnbListing.getMinimumNights()).equals(lowerCaseFilter));
+            });
+        });
+
+        //Wrap the FilteredList in a SortedList
+        SortedList<AirbnbListing> sortedData = new SortedList<>(filteredData);
+
+        sortedData.comparatorProperty().bind(table.comparatorProperty());
+
+        table.setItems(sortedData);
     }
 
     /**
@@ -175,6 +204,11 @@ public class MapPanelEngine
             default:
                 System.out.println("Column not found! No Action");
                 break;
+        }
+        boroughListings.clear();
+        for (Object obj : table.getItems()) {
+            AirbnbListing listing = (AirbnbListing) obj;
+            boroughListings.add(listing);
         }
     }
 }
---
+++ StatisticPanel.java
@@ -0,0 +1,245 @@
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.scene.Node;
+import javafx.collections.FXCollections;  
+import javafx.collections.ObservableList; 
+import javafx.geometry.Pos;
+import javafx.event.*;
+import javafx.scene.image.*;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * This Panel contains four statistic boxes which display statistics 
+ * about the airbnb listings.
+ * The user can click between different statistics in each box.
+ * The same statistic cannot be shown in two or more separate panels 
+ * at the same time.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class StatisticPanel
+{  
+    private List<DataDisplay> data = new ArrayList<>();
+    private List<GridPane> statBoxes = new ArrayList<>();
+    private static Statistics stats;
+    private ListingManager listingManager;
+    private GridPane statPane;
+    private StackPane stackPane;    // Used to show the large statistic
+    private GridPane enlargedStatBox;   // This is used to store the box which had the statistic in it previously
+    private final VBox enlargedStatMessage = new VBox();
+    
+    public StatisticPanel(ArrayList<AirbnbListing> listings, ListingManager listingManager)
+    {
+        this.listingManager = listingManager;
+        stackPane = new StackPane();
+        statPane = new GridPane();  
+        enlargedStatBox = null;
+        stats = new Statistics(listings);
+        createStatisticPanel();
+    }  
+
+    public void createStatisticPanel() 
+    {
+        // Create a new grid pane
+        statPane = new GridPane();
+        statPane.setPadding(new Insets(0, 10, 0, 10));
+        statPane.setVgap(2);
+        statPane.setHgap(2);
+        
+        for (int i=0; i<2 ; i++) {
+            ColumnConstraints column = new ColumnConstraints();
+            column.setPercentWidth(50);
+            column.setHgrow(Priority.ALWAYS);
+            statPane.getColumnConstraints().add(column);
+        }
+        
+        for (int i=0; i<2 ; i++) {
+            RowConstraints row = new RowConstraints();
+            row.setPercentHeight(50);
+            row.setVgrow(Priority.ALWAYS);
+            statPane.getRowConstraints().add(row);
+        }        
+        //pane.setGridLinesVisible(true); // uncomment to show gridlines
+        
+        //createData();
+        
+        createStatisticBoxes();
+        
+        enlargedStatMessage();
+        
+        stackPane.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        stackPane.getChildren().add(statPane);
+    }
+    
+    public void updateStatistics(ArrayList<AirbnbListing> listings)
+    {
+        stats.updateStatistics(listings);
+        data.clear();
+        createData();
+        for (GridPane box : statBoxes) {
+            statPane.getChildren().remove(box);
+        }
+        createStatisticBoxes();
+    }
+    
+    private void createStatisticBoxes()
+    {
+        GridPane statBox1 = new StatisticBox(data).getStatBox();        
+        GridPane statBox2 = new StatisticBox(data).getStatBox(); 
+        GridPane statBox3 = new StatisticBox(data).getStatBox();        
+        GridPane statBox4 = new StatisticBox(data).getStatBox();
+        
+        statBoxes.add(statBox1);        
+        statBoxes.add(statBox2);  
+        statBoxes.add(statBox3);  
+        statBoxes.add(statBox4);          
+        
+        // Add the statBoxes to the panel
+        statPane.add(statBox1, 0, 0);
+        statPane.add(statBox2, 1, 0);
+        statPane.add(statBox3, 0, 1);
+        statPane.add(statBox4, 1, 1);
+    }
+    
+    /**
+     * This creates the data of type DataDisplay and stores it in an
+     * arraylist
+     */
+    private void createData()
+    {
+        data.add(new BasicStatisticDisplay(this,"Average Number of Reviews", stats.getAvgNumOfReviewsString()));
+        data.add(new BasicStatisticDisplay(this,"Total Available Properties", stats.getTotalAvailPropertiesString()));
+        data.add(new BasicStatisticDisplay(this,"Number of Entire Homes\nand Apartments", stats.getNumOfHomesAndAptsString()));
+        data.add(new BasicStatisticDisplay(this,"Most Expensive Borough", stats.getMostExpBorough()));
+        data.add(new PieChartDisplay(this, "Number of Properties in each Borough", stats.propertiesInBorough()));
+        data.add(new BarChartDisplay(this, "Room types in Borough ", "roomtypes", listingManager.getBoroughOptions()));
+        data.add(new BarChartDisplay(this, "Availabilty in Borough", "availability", listingManager.getBoroughOptions()));
+        data.add(new BasicStatisticDisplay(this, "Statistic 8", "bruh#4"));
+    }
+    
+    public void enlargedStatMessage()
+    {
+        Image icon = new Image("/images/pepega.png");
+        ImageView iconView = new ImageView();
+        iconView.setImage(icon);
+        iconView.setPreserveRatio(true);
+        iconView.setFitHeight(100);
+        iconView.setFitWidth(100);
+                
+        final Label tempLabel = new Label("This statistic is currently enlarged");
+        tempLabel.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        tempLabel.setAlignment(Pos.CENTER);
+        enlargedStatMessage.setVgrow(tempLabel, Priority.ALWAYS);
+        enlargedStatMessage.setAlignment(Pos.CENTER);
+        enlargedStatMessage.getChildren().addAll(tempLabel, iconView);   
+    }
+    
+    public StatisticPanel getStatisticPanelClass()
+    {
+        return this;
+    }    
+    
+    public void showEnlargedStat(GridPane stat)
+    {
+        boolean done = false;
+                
+        for (GridPane box : statBoxes){
+            ObservableList<Node> list = FXCollections.observableArrayList();
+            list = (ObservableList<Node>)box.getChildren();
+            for (Node node : list){
+                if (node instanceof GridPane) {
+                    GridPane node1 = (GridPane) node;
+                    if (node1 == stat) {
+                        //node.setVisible(false);                        
+                        box.getChildren().remove(stat);                        
+                        box.add(enlargedStatMessage, 1, 0); 
+                        enlargedStatBox = box;
+                        done = true;
+                    }
+                    break;
+                }
+            } 
+            if (done){
+                break;
+            }
+        }       
+        
+        stat.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);        
+        VBox container = new VBox(); 
+        GridPane backgroundDim = new GridPane();
+        backgroundDim.setPrefSize(statPane.getWidth(), statPane.getHeight());
+        backgroundDim.setStyle("-fx-background-color: rgba(0, 0, 0, 0.2)");
+        GridPane largeStat = new GridPane();        
+        
+        largeStat.setMaxSize((statPane.getWidth() * 0.75), (statPane.getHeight() * 0.75));
+        largeStat.getChildren().add(stat);
+        largeStat.toFront();
+        largeStat.setVgrow(stat, Priority.ALWAYS);
+        largeStat.setHgrow(stat, Priority.ALWAYS);
+        largeStat.setAlignment(Pos.CENTER);        
+        largeStat.setStyle("-fx-background-color: rgba(255, 255, 255, 0.9)");         
+        
+        Button close = new Button("Close");
+        close.setAlignment(Pos.CENTER);
+        close.setPrefSize(100, 30);
+        close.setOnAction(e -> removeLargeStat(stat));
+        
+        container.setMaxSize((statPane.getWidth() * 0.75), (statPane.getHeight() * 0.75));
+        container.setPrefSize((statPane.getWidth() * 0.75), (statPane.getHeight() * 0.75));
+        container.toFront();
+        container.setVgrow(largeStat, Priority.ALWAYS);
+        container.setAlignment(Pos.CENTER);
+        container.getChildren().addAll(largeStat, close);
+        
+        
+        if (stackPane.getChildren().size() == 1)
+        {
+            showLargeStat(backgroundDim, container);
+        }
+        else{
+            removeStackLayers();
+            showLargeStat(backgroundDim, container);
+        }     
+    }
+    
+    private void removeLargeStat(Pane stat)
+    {
+        if (stackPane.getChildren().size() >= 2) {
+            removeStackLayers();          
+        }
+        enlargedStatBox.getChildren().remove(enlargedStatMessage);
+        enlargedStatBox.add(stat, 1, 0);
+    }
+    
+    private void showLargeStat(GridPane backgroundDim, VBox container)
+    {
+        stackPane.getChildren().add(backgroundDim);
+        stackPane.getChildren().add(container);
+    }
+    
+    private void removeStackLayers()
+    {
+        int num = stackPane.getChildren().size() - 1;
+        while (stackPane.getChildren().size() > 1) {
+            stackPane.getChildren().remove(num);
+            num--;            
+        }
+    }
+    
+    public Statistics getStatistics()
+    {
+        return stats;
+    }
+    
+    public StackPane getStatisticPanel()
+    {
+        return stackPane;
+    }
+}
